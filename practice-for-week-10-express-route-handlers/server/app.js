6594878b50c72ac8bbf61bb36dc109c5fc646f80
George Merida <gmerida92@gmail.com>
2023-09-12 19:39:25 -0700

    Start Bonus

--- practice-for-week-10-express-route-handlers/server/app.js
+++ practice-for-week-10-express-route-handlers/server/app.js
@@ -27,14 +27,33 @@ const app = express();
 // Your code here
 app.use(express.json())
 
-app.use((req, res, next) => {
-  console.log('Request Body:', req.body);
-  next();
-});
+// app.use((req, res, next) => {
+//   console.log('Request Body:', req.body);
+//   next();
+// });
 
 app.get('/artists', (req, res, next) => {
   let allArtists = getAllArtists();
   return res.json(allArtists);
+})
+
+app.post('/artists', (req, res, next) => {
+  let newArtist = req.body
+  // console.log(newArtist, req.body)
+  res.status(201)
+  return res.json(addArtist(newArtist))
+})
+
+app.get('/artists/latest', (req, res, next) => {
+  let latestArtist = getLatestArtist();
+  res.status(200);
+  return res.json(latestArtist)
+})
+
+app.get('/artists/latest/albums', (req, res, next) => { 
+  let albumFromLatestArtist = getAlbumsForLatestArtist();
+  res.status(200);
+  return res.json(albumFromLatestArtist);
 })
 
 // DO NOT MODIFY
