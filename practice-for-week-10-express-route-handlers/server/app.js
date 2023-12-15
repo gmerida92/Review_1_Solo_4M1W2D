eb49024ea65890ff613e46d8550f9a9c18e2910d
George Merida <gmerida92@gmail.com>
2023-12-14 17:22:35 -0800

    Add Comments

--- practice-for-week-10-express-route-handlers/server/app.js
+++ practice-for-week-10-express-route-handlers/server/app.js
@@ -33,31 +33,36 @@ app.use(express.json())
 //   next();
 // });
 
+
+// README.md: Get all the artists
 app.get('/artists', (req, res, next) => {
   let allArtists = getAllArtists();
-  // console.log("HERE HI")
   return res.json(allArtists);
 })
 
+
+// README.md: Add an artist
 app.post('/artists', (req, res, next) => {
   let newArtist = req.body
-  // console.log(newArtist, req.body)
   res.status(201)
   return res.json(addArtist(newArtist))
 })
 
+// README-bonus.md: Get the latest artis added
 app.get('/artists/latest', (req, res, next) => {
   let latestArtist = getLatestArtist();
   res.status(200);
   return res.json(latestArtist)
 })
 
+// README-bonus.md: Get all albums of the latest artist
 app.get('/artists/latest/albums', (req, res, next) => {
   let albumFromLatestArtist = getAlbumsForLatestArtist();
   res.status(200);
   return res.json(albumFromLatestArtist);
 })
 
+// README-long-practice.md: Get a specific artist's details based on artistId
 app.get('/artists/:artistId', (req, res, next) => {
   let { artistId } = req.params;
   let artistById = getArtistByArtistId(artistId);
@@ -65,9 +70,9 @@ app.get('/artists/:artistId', (req, res, next) => {
   return res.json(artistById)
 })
 
+// README-long-practice.md: Edit a specific artist by artist Id
 app.put('/artists/:artistId', (req, res, next) => {
   let { artistId } = req.params;
-  // console.log(artistId)
   let { name } = req.body;
 
   let artistEdits = {
@@ -75,10 +80,8 @@ app.put('/artists/:artistId', (req, res, next) => {
     name: name
   }
 
-  // console.log("HERE1", artistEdits)
 
   let updatedArtist = editArtistByArtistId(artistId, artistEdits)
-  // console.log("HERE2", updatedArtist)
   res.status(200);
   return res.json(updatedArtist);
 })
