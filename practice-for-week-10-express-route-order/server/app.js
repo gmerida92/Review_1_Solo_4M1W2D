644d057c403d0841774d484592107b4c3fab09d6
George Merida <gmerida92@gmail.com>
2024-01-09 08:15:06 -0800

    Commit 1

--- practice-for-week-10-express-route-order/server/app.js
+++ practice-for-week-10-express-route-order/server/app.js
@@ -9,10 +9,6 @@ app.get('/hello', (req, res) => {
     res.send("Hello, my friend!");
 });
 
-app.get(['/goodbye', '/goodbye/*'], (req, res) => {
-    res.send("Goodbye, my friend!");
-});
-
 app.get('/goodbye/until/:time', (req, res) => {
     res.send(`Goodbye. See you ${req.params.time}.`);
 });
@@ -20,6 +16,12 @@ app.get('/goodbye/until/:time', (req, res) => {
 app.get('/goodbye/until/forever', (req, res) => {
     res.send("So long. Farewell. Have a great life!");
 });
+
+app.get(['/goodbye', '/goodbye/*'], (req, res) => {
+    res.send("Goodbye, my friend!");
+});
+
+
 
 const port = 5000;
 app.listen(port, () => console.log('Server is listening on port', port));
